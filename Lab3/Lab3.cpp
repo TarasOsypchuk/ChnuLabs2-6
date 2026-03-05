e549f0004979bd1a22bf2ef860f44fe5526960f4
Taras <worktarasosipchuk@gmail.comgit config --global user.email worktarasosipchuk@gmail.com>
2026-03-05 20:39:47 +0200

    improve class BeeBatch and add more functional

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -43,20 +43,33 @@ public:
 	friend istream& operator>>(istream& is, BeeHive& hive);
 };
 
+int BeeHive::totalHivesCount = 0;
+
+ostream& operator<<(ostream& os, const BeeHive& hive) {
+	os << "Hive" << hive.material << "  Bees:" << hive.beeCount << endl;
+	return os;
+}
+
+istream& operator>>(istream& is, BeeHive& hive) {
+	cout << "Enter material and bee count: ";
+	is >> hive.material >> hive.beeCount;
+	return is;
+}
+
 class BeeKeeper {
 private:
 	string name;
 	int experience;
 
 public:
-	BeeKeeper(string n, int exp) : name(n), experience(exp) {
-
-		cout << "BeeKepeer " << name << " working" << endl;
+	BeeKeeper(string name, int experience) {
+		this->name = name;
+		this->experience = experience;
+		cout << "BeeKepeer " << this->name << " working" << endl;
 
 	}
 
 	~BeeKeeper() {
-
 		cout << "BeeKeeper " << name << " is resting" << endl;
 
 	}
@@ -74,50 +87,60 @@ class HoneyBatch {
 private:
 
 	double liters;
-
 	string flowerType;
+
+	string* batchNote;
 
 public:
 
 	HoneyBatch(double l, string type = "mixed") : liters(l), flowerType(type) {
-
+		batchNote = new string("Standart Quality");
 		cout << "HoneyBatch was collected - " << liters << "l honey(" << flowerType << ")" << endl;
 
 	}
 
-	HoneyBatch() : HoneyBatch(1, "test") {
-
-		cout << "HoneyButch test production" << endl;
-
+	HoneyBatch(const HoneyBatch& other) : liters(other.liters), flowerType(other.flowerType) {
+		batchNote = new string(*other.batchNote + " (Copy)");
+		cout << "Copy constructor called for " << flowerType << "batch" << endl;
+	}
+	HoneyBatch(HoneyBatch&& other)noexcept
+		: liters(other.liters), flowerType(move(other.flowerType)), batchNote(other.batchNote) {
+		other.batchNote = nullptr;
+		cout << "Move constructor called for " << flowerType << endl;
 	}
 
+	~HoneyBatch() {
+		delete batchNote;
+	}
+	HoneyBatch operator+(const HoneyBatch& other) {
+		return HoneyBatch(this->liters + other.liters, this->flowerType + "&" + other.flowerType);
+	}
+
+	void printInfo() const {
+		cout << "Batch: " << flowerType << " - " << liters << " liters" << endl;
+	}
 };
 
 
 
 int main() {
 
-	BeeHive mainHive(10000, "wood", 35.2);
+	cout << "Hives count: " << BeeHive::getTotalHives() << endl;
 
-	BeeHive emptyHive;
+	BeeHive mainHive(999, "wood", 35.9);
 
-	emptyHive.showStatus();
+	BeeHive ecoHive(3000, "Straw", 32);
+	ecoHive.showStatus();
+	++mainHive;
 
+	cout << "Updated hive: " << mainHive << endl;
+	HoneyBatch acacia(10, "Acacia");
+	HoneyBatch acaciaCopy = acacia;
+	HoneyBatch merged = acacia + HoneyBatch(5, "Rice");
 
+	merged.printInfo();
+	cout << "Total Hives: " << BeeHive::getTotalHives() << endl;
 
-	HoneyBatch batch1(5.5, "Acacia");
-
-	HoneyBatch batch2(3);
-
-	HoneyBatch testBatch;
-
-	{
-
-		BeeKeeper keeper("JunJun", 99);
-
-		keeper.collectHoney();
-
-	}
 
 	return 0;
 
