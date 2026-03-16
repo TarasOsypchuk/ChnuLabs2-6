82ac9730404525a34508754b8178910d75627991
Taras <worktarasosipchuk@gmail.comgit config --global user.email worktarasosipchuk@gmail.com>
2026-03-16 14:34:16 +0200

    make 4lab

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -3,145 +3,202 @@
 
 using namespace std;
 
-class BeeHive {
+class Sensor {
 private:
+    string type;
+    double value;
+public:
+    Sensor(string t = "Temperature", double v = 0.0) : type(t), value(v) {
+        cout << "   [Sensor] " << type << " initialized." << endl;
+    }
+    ~Sensor() {
+        cout << "   [Sensor] " << type << " destroyed." << endl;
+    }
+    double getValue() const { return value; }
+};
 
-	int beeCount;
-	string material;
-	double temperature;
+class Person {
+protected:
+    string name;
+public:
+    Person(string n) : name(n) {
+        cout << "[Person] Constructor called for " << name << endl;
+    }
+    virtual ~Person() {
+        cout << "[Person] Destructor called for " << name << endl;
+    }
+    virtual void work() = 0;
+};
 
-	static int totalHivesCount;
+class BeeKeeper : public Person {
+private:
+    int experience;
+public:
+    BeeKeeper(string name, int experience) : Person(name), experience(experience) {
+        cout << "[BeeKeeper] " << this->name << " is ready to work." << endl;
+    }
+    ~BeeKeeper() override {
+        cout << "[BeeKeeper] " << name << " is resting." << endl;
+    }
+    void work() override {
+        cout << name << " is collecting honey in the apiary." << endl;
+    }
+};
+
+class BeeHive {
+protected:
+    int beeCount;
+    string material;
+    double temperature;
+    static int totalHivesCount;
 
 public:
-	BeeHive(int count, string mat, double temp)
+    BeeHive(int count, string mat, double temp)
+        : beeCount(count), material(mat), temperature(temp) {
+        totalHivesCount++;
+        cout << "[BeeHive] Created of (" << material << ")." << endl;
+    }
+    BeeHive() : beeCount(0), material("Plastic"), temperature(20.0) {
+        totalHivesCount++;
+        cout << "[BeeHive] Created empty." << endl;
+    }
 
-		: beeCount(count), material(mat), temperature(temp) {
-		totalHivesCount++;
-		cout << "BeeHive create of(" << material << ")." << endl;
+    virtual ~BeeHive() {
+        totalHivesCount--;
+        cout << "[BeeHive] Destroyed." << endl;
+    }
 
-	}
-	BeeHive() : beeCount(0), material("Plastic"), temperature(20.0) {
+    static int getTotalHives() { return totalHivesCount; }
 
-		cout << "BeeHive created empty" << endl;
+    virtual void showStatus() const {
+        cout << "BeeHive : " << beeCount << " bees, Material " << material << endl;
+    }
 
-	}
-	static int getTotalHives() {
-		return totalHivesCount;
-	}
-
-	void showStatus() {
-
-		cout << "BeeHive : " << beeCount << " bees, Material " << material << endl;
-
-	}
-
-	BeeHive& operator++() {
-		this->beeCount += 500;
-		return *this;
-	}
-	friend ostream& operator<<(ostream& os, const BeeHive& hive);
-	friend istream& operator>>(istream& is, BeeHive& hive);
+    BeeHive& operator++() {
+        this->beeCount += 500;
+        return *this;
+    }
+    friend ostream& operator<<(ostream& os, const BeeHive& hive);
 };
 
 int BeeHive::totalHivesCount = 0;
 
 ostream& operator<<(ostream& os, const BeeHive& hive) {
-	os << "Hive" << hive.material << "  Bees:" << hive.beeCount << endl;
-	return os;
+    os << "Hive " << hive.material << " | Bees:" << hive.beeCount;
+    return os;
 }
 
-istream& operator>>(istream& is, BeeHive& hive) {
-	cout << "Enter material and bee count: ";
-	is >> hive.material >> hive.beeCount;
-	return is;
-}
-
-class BeeKeeper {
+class SmartHive : public BeeHive {
 private:
-	string name;
-	int experience;
+    string* ipAddress;
+    Sensor tempSensor;
 
 public:
-	BeeKeeper(string name, int experience) {
-		this->name = name;
-		this->experience = experience;
-		cout << "BeeKepeer " << this->name << " working" << endl;
+    SmartHive(int count, string mat, double temp, string ip)
+        : BeeHive(count, mat, temp), tempSensor("Temperature", temp) {
+        ipAddress = new string(ip);
+        cout << "[SmartHive] Connected to IP: " << *ipAddress << endl;
+    }
 
-	}
+    SmartHive(const SmartHive& other)
+        : BeeHive(other), tempSensor(other.tempSensor) {
+        ipAddress = new string(*other.ipAddress + " (Copy)");
+        cout << "[SmartHive] Copy constructor called." << endl;
+    }
 
-	~BeeKeeper() {
-		cout << "BeeKeeper " << name << " is resting" << endl;
+    SmartHive(SmartHive&& other) noexcept
+        : BeeHive(move(other)), tempSensor(move(other.tempSensor)), ipAddress(other.ipAddress) {
+        other.ipAddress = nullptr;
+        cout << "[SmartHive] Move constructor called." << endl;
+    }
 
-	}
+    SmartHive& operator=(const SmartHive& other) {
+        cout << "[SmartHive] Copy assignment operator called." << endl;
+        if (this != &other) {
+            BeeHive::operator=(other);
+            tempSensor = other.tempSensor;
+            delete ipAddress;
+            ipAddress = new string(*other.ipAddress);
+        }
+        return *this;
+    }
 
-	void collectHoney() {
+    SmartHive& operator=(SmartHive&& other) noexcept {
+        cout << "[SmartHive] Move assignment operator called." << endl;
+        if (this != &other) {
+            BeeHive::operator=(move(other));
+            tempSensor = move(other.tempSensor);
+            delete ipAddress;
+            ipAddress = other.ipAddress;
+            other.ipAddress = nullptr;
+        }
+        return *this;
+    }
 
-		cout << name << " is collecting honey" << endl;
+    ~SmartHive() override {
+        delete ipAddress;
+        cout << "[SmartHive] Destroyed." << endl;
+    }
 
-	}
-
+    void showStatus() const override {
+        cout << "SmartHive [" << (ipAddress ? *ipAddress : "Offline") << "] : "
+            << beeCount << " bees, Temp: " << tempSensor.getValue() << "C" << endl;
+    }
 };
 
 class HoneyBatch {
-
 private:
-
-	double liters;
-	string flowerType;
-
-	string* batchNote;
+    double liters;
+    string flowerType;
+    string* batchNote;
 
 public:
+    HoneyBatch(double l, string type = "mixed") : liters(l), flowerType(type) {
+        batchNote = new string("Standard Quality");
+        cout << "[HoneyBatch] Collected - " << liters << "l (" << flowerType << ")" << endl;
+    }
 
-	HoneyBatch(double l, string type = "mixed") : liters(l), flowerType(type) {
-		batchNote = new string("Standart Quality");
-		cout << "HoneyBatch was collected - " << liters << "l honey(" << flowerType << ")" << endl;
+    HoneyBatch(const HoneyBatch& other) : liters(other.liters), flowerType(other.flowerType) {
+        batchNote = new string(*other.batchNote + " (Copy)");
+    }
 
-	}
+    HoneyBatch(HoneyBatch&& other) noexcept
+        : liters(other.liters), flowerType(move(other.flowerType)), batchNote(other.batchNote) {
+        other.batchNote = nullptr;
+    }
 
-	HoneyBatch(const HoneyBatch& other) : liters(other.liters), flowerType(other.flowerType) {
-		batchNote = new string(*other.batchNote + " (Copy)");
-		cout << "Copy constructor called for " << flowerType << "batch" << endl;
-	}
-	HoneyBatch(HoneyBatch&& other)noexcept
-		: liters(other.liters), flowerType(move(other.flowerType)), batchNote(other.batchNote) {
-		other.batchNote = nullptr;
-		cout << "Move constructor called for " << flowerType << endl;
-	}
+    ~HoneyBatch() { delete batchNote; }
 
-	~HoneyBatch() {
-		delete batchNote;
-	}
-	HoneyBatch operator+(const HoneyBatch& other) {
-		return HoneyBatch(this->liters + other.liters, this->flowerType + "&" + other.flowerType);
-	}
+    HoneyBatch operator+(const HoneyBatch& other) {
+        return HoneyBatch(this->liters + other.liters, this->flowerType + "&" + other.flowerType);
+    }
 
-	void printInfo() const {
-		cout << "Batch: " << flowerType << " - " << liters << " liters" << endl;
-	}
+    void printInfo() const {
+        cout << "Batch: " << flowerType << " - " << liters << " liters" << endl;
+    }
 };
 
-
-
 int main() {
+    cout << "--- 1. Testing Person and BeeKeeper (Upward Hierarchy) ---" << endl;
+    BeeKeeper master("Taras", 5);
+    master.work();
 
-	cout << "Hives count: " << BeeHive::getTotalHives() << endl;
+    cout << "\n--- 2. Testing SmartHive and Composition (Downward & Has-A) ---" << endl;
+    SmartHive node1(50000, "Wood", 34.5, "192.168.1.10");
+    node1.showStatus();
 
-	BeeHive mainHive(999, "wood", 35.9);
+    cout << "\n--- 3. Testing Rule of 5 in Derived Class ---" << endl;
+    SmartHive node2 = node1;
+    SmartHive node3(10000, "Plastic", 33.0, "192.168.1.11");
+    node3 = move(node1);
 
-	BeeHive ecoHive(3000, "Straw", 32);
-	ecoHive.showStatus();
-	++mainHive;
+    cout << "\n--- 4. Original Functionality Check ---" << endl;
+    HoneyBatch acacia(10, "Acacia");
+    HoneyBatch merged = acacia + HoneyBatch(5, "Linden");
+    merged.printInfo();
 
-	cout << "Updated hive: " << mainHive << endl;
-	HoneyBatch acacia(10, "Acacia");
-	HoneyBatch acaciaCopy = acacia;
-	HoneyBatch merged = acacia + HoneyBatch(5, "Rice");
+    cout << "\nTotal Hives active: " << BeeHive::getTotalHives() << endl;
 
-	merged.printInfo();
-	cout << "Total Hives: " << BeeHive::getTotalHives() << endl;
-
-
-	return 0;
-
+    cout << "\n--- 5. Destructor Sequence Check ---" << endl;
+    return 0;
 }
